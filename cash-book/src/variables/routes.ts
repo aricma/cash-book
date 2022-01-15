77f2388e5eeb2baa13a595081be4d45f2554c1bc
aricma <adrian@aricma.com>
2022-01-15 23:41:45 +0100

    refactor: rename routes

--- cash-book/src/variables/routes.ts
+++ cash-book/src/variables/routes.ts
@@ -1,4 +1,4 @@
-export const ROUTES_CREATE_BOOK_ENTRY = '/today';
+export const ROUTES_CREATE_BOOK_ENTRY = '/book-entries/create';
 export const ROUTES_ACCOUNTS = '/accounts';
 export const ROUTES_TRANSACTIONS = '/transactions';
-export const ROUTES_BOOK_ENTRIES = '/bookings';
+export const ROUTES_BOOK_ENTRIES = '/book-entries';

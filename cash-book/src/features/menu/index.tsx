9fde80d9d8d70bca782077713916687b4067e834
aricma <adrian@aricma.com>
2022-01-16 12:31:43 +0100

    change: reset button icon

--- cash-book/src/features/menu/index.tsx
+++ cash-book/src/features/menu/index.tsx
@@ -78,7 +78,7 @@ export const Menu: React.FC = () => {
 			},
 			{
 				type: 'BUTTON_PROPS_TYPE',
-				icon: IconType.DEFAULT,
+				icon: IconType.CLOSE_FILL,
 				title: 'Reset',
 				onSelect: () => {
 					window.confirm("Do you really want to reset your app? Everything will be deleted! This action can not be undone!")

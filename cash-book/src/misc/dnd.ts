2fe9c358c82fa4d102547ce8e3a07909152049fd
aricma <adrian@aricma.com>
2022-01-22 10:23:46 +0100

    change: disable drag and drop for touch devices

--- cash-book/src/misc/dnd.ts
+++ cash-book/src/misc/dnd.ts
@@ -1,12 +1,12 @@
-import {TouchBackend} from 'react-dnd-touch-backend';
+// import {TouchBackend} from 'react-dnd-touch-backend';
 import {HTML5Backend} from 'react-dnd-html5-backend';
 
 
 export const dndBackend = () => {
-    return isTouchDevice() ? TouchBackend : HTML5Backend;
+    return HTML5Backend; // isTouchDevice() ? TouchBackend : HTML5Backend;
 };
 
-const isTouchDevice = () => {
-    // @ts-ignore
-    return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
-}
+// const isTouchDevice = () => {
+//     // @ts-ignore
+//     return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+// }

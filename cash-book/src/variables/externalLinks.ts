b1096cb70616b99cd9917a761c58821337b778b6
aricma <adrian@aricma.com>
2022-01-29 17:46:45 +0100

    add: external link for the support

--- cash-book/src/variables/externalLinks.ts
+++ cash-book/src/variables/externalLinks.ts
@@ -1,2 +1,3 @@
 export const DOCS_QUICK_START = "https://aricma.gitbook.io/cashbook/guides/quickstart";
 export const DOCS_ACCOUNTS = "https://aricma.gitbook.io/cashbook/fundamentals/accounts";
+export const DOCS_SUPPORT = "https://aricma.gitbook.io/cashbook/support/faq";

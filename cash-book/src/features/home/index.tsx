1f60d988cde58b5f813e6fdd09fa8e3d8761ca50
aricma <adrian@aricma.com>
2022-01-29 13:54:36 +0100

    add: loading animation to the home view

--- cash-book/src/features/home/index.tsx
+++ cash-book/src/features/home/index.tsx
@@ -5,24 +5,46 @@ import {ROUTES_CREATE_BOOK_ENTRY} from '../../variables/routes';
 import {Icon} from '../../components/icons';
 import {IconType} from '../../models/props';
 
+const timeouts = [100, 200, 300, 50, 200, 1000, 50];
 
 export const Home: React.FC = () => {
+    const [loading, setLoading] = React.useState<number>(0);
     React.useEffect(() => {
-        setTimeout(() => {
-            dispatch({
-                type: ApplicationActionType.ROUTER_GO_TO,
-                path: ROUTES_CREATE_BOOK_ENTRY,
-            });
-        }, 1500);
+        const sleep = setTimeout(() => {
+            if (timeouts.length === loading) {
+                dispatch({
+                    type: ApplicationActionType.ROUTER_GO_TO,
+                    path: ROUTES_CREATE_BOOK_ENTRY,
+                });
+            } else {
+                setLoading(prev => prev + 1);
+            }
+        }, timeouts[loading]);
+        return () => clearTimeout(sleep);
         // eslint-disable-next-line
-    }, []);
+    }, [loading]);
 
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen bg-blue-100 flex items-center justify-center">
-            <h1 className="text-blue-600 font-extrabold text-4xl flex items-center">
-                <span>cash</span>
-                <Icon type={IconType.CASH_BOOK} className="w-[4rem]" />
-                <span>Book</span>
+        <div className="fixed top-0 left-0 w-screen h-screen bg-blue-100 flex flex-col items-center justify-center space-y-4">
+            <h1 className="text-blue-600 font-extrabold text-4xl grid grid-cols-[repeat(3,_max-content)] grid-rows-[repeat(2,_max-content)]">
+                <Icon type={IconType.CASH_BOOK} className="row-span-2 w-[4rem]" />
+                <span className="self-end justify-self-end">cash</span>
+                <span className="self-end justify-self-start">Book</span>
+                <div className="place-self-center col-start-2 col-span-2 flex space-x-1">
+                    {
+                        timeouts.map((_, index) => (
+                            <React.Fragment key={index}>
+                                {
+                                    index <= loading ? (
+                                        <div className="h-2 w-5 bg-blue-600 rounded-md" />
+                                    ) : (
+                                        <div className="h-2 w-5 bg-blue-200 rounded-md" />
+                                    )
+                                }
+                            </React.Fragment>
+                        ))
+                    }
+                </div>
             </h1>
         </div>
     );
